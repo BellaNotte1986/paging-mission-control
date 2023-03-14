c279a2894f6f405af1fd1729eac0b375a2abd288
Tat Chan <77873239+tchan102@users.noreply.github.com>
2023-03-14 01:33:57 -0700

    changed the createRecord function, to include severity

--- src/paging_mission_control.py
+++ src/paging_mission_control.py
@@ -9,36 +9,53 @@ def fetchTxtFile(filename: str) -> list[str]:
   return status_telemetry_data
 
 def createTimestamp(timestamp: str) -> datetime:
+  '''convert a string of time into a timestamp'''
   date, time = timestamp.split(" ")
   year, month, day = date[:4], date[4:6], date[6:]
   result = f"{year}-{month}-{day} {time}"
   return datetime.fromisoformat(result)
 
+def createSeverity(red_high_limit:float, yellow_high_limit:float, yellow_low_limit:float, red_low_limit:float, raw_value: float) -> str:
+  if raw_value > red_high_limit:
+    return "RED HIGH"
+  elif raw_value > yellow_high_limit:
+    return "YELLOW HIGH"
+  elif raw_value < red_low_limit:
+    return "RED LOW"
+  elif raw_value < yellow_low_limit:
+    return "YELLOW LOW"
+  return "NORMAL"
+
 def createRecord(row: str) -> dict:
   timestamp, satellite_id, red_high_limit, yellow_high_limit, yellow_low_limit, red_low_limit, raw_value, component = row.split('|')
+  
   record = {
-      'timestamp': createTimestamp(timestamp),
-      'satelliteId': satellite_id,
-      'red_high_limit': float(red_high_limit), 
-      'yellow_high_limit': float(yellow_high_limit),
-      'yellow_low_limit': float(yellow_low_limit),
-      'red_low_limit': float(red_low_limit),
-      'raw_value': float(raw_value),
+      'satelliteId': int(satellite_id),
+      'severity': createSeverity(float(red_high_limit), float(yellow_high_limit), float(yellow_low_limit), float(red_low_limit), float(raw_value)),
       'component': component,
+      'timestamp': createTimestamp(timestamp),
     }
+  
   return record
 
 def fileToData(data: list[str]) -> list[dict]:
-  result = list()
-  
-  for status in data:  
-    result.append(createRecord(status))
+  result = [createRecord(status) for status in data]
   return result
 
+def createAlert(status: dict) -> dict:
+  
+  return {}
 def alert(data: list[dict]) -> list[dict]:
+  alert_list = list()
+  result = list()
+  battery_voltage_below_red_low = dict()
+  temperature_reading_exceed_red_high = dict()
   pass
-# print(os.getcwd())
+
+  
+
 data = fetchTxtFile("input.txt")
 data = fileToData(data)
-print(data[0])
-print(data[0]['timestamp'].isoformat())
+for line in data:
+  print(line)
+# print(data[0]['timestamp'].isoformat())

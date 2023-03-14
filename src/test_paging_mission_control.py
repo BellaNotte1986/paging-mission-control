211e5723cf0355dec2faf67619627999bc4968e1
Tat Chan <77873239+tchan102@users.noreply.github.com>
2023-03-14 01:33:12 -0700

    added sample test case

--- src/test_paging_mission_control.py
+++ src/test_paging_mission_control.py
@@ -1,3 +1,14 @@
 import paging_mission_control
 import unittest
 
+class TestPagingMissionControl(unittest.TestCase):
+  
+  def test_createSeverity(self):
+    self.assertEqual(paging_mission_control.createSeverity(10,5,2,1,-1), "RED LOW")
+    self.assertEqual(paging_mission_control.createSeverity(10,5,2,1,1.5), "YELLOW LOW")
+    self.assertEqual(paging_mission_control.createSeverity(10,5,2,1,11), "RED HIGH")
+    self.assertEqual(paging_mission_control.createSeverity(10,5,2,1,6), "YELLOW HIGH")
+    self.assertEqual(paging_mission_control.createSeverity(10,5,2,1,5), "NORMAL")
+    
+if __name__ == '__main__':
+  unittest.main()

287bf40a11797131b82fd7de328bdaac9558d804
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-03-13 22:47:22 -0400

    Read input file into a `list[Record]`
    The red and yellow bounds seem to be constant for each satellite. We
    might be able to optimize the parsing slightly by only storing the raw
    value and component when we already have the satellite values. This
    should do for now, though.

--- solve.py
+++ solve.py
@@ -2,6 +2,7 @@ import sys
 from dataclasses import dataclass
 import datetime as dt
 import enum
+from operator import attrgetter
 
 
 class Component(enum.StrEnum):
@@ -34,6 +35,40 @@ def parse_timestamp(s: str) -> dt.datetime:
 
     # timestamp is annoyingly nonstandard
     return dt.datetime.strptime(s, "%Y%m%d %H:%M:%S.%f")
+
+
+def read_records() -> list[Record]:
+    input_file = sys.argv[1]  # first is filename
+    records = []
+    with open(input_file) as f:
+        for line in map(str.strip, f):
+            (
+                ts,
+                sid,
+                red_high,
+                yellow_high,
+                yellow_low,
+                red_low,
+                val,
+                component,
+            ) = line.split("|")
+            records.append(
+                Record(
+                    timestamp=parse_timestamp(ts),
+                    satellite_id=int(sid),
+                    red_high=int(red_high),
+                    yellow_high=int(yellow_high),
+                    yellow_low=int(yellow_low),
+                    red_low=int(red_low),
+                    val=float(val),
+                    component=Component(component),
+                )
+            )
+
+    # not sure if sorting is necessary. the test data is ordered, but it could
+    # be possible that data may arrive out of order. if it's ordered anyways,
+    # we don't pay that much since timsort can handle it relatively quickly
+    return sorted(records, key=attrgetter("timestamp"))
 
 
 if len(sys.argv) != 2:

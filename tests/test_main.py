95855e8f48664016dccdafc4d8b3aeea79433d5d
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-03-14 18:42:29 -0400

    Add test for `read_records`

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,10 @@
 import datetime as dt
+from pathlib import Path
 
 import pytest
 
 from mission_control import __main__
+from mission_control.process_data import Component, Record
 
 TEST_INPUT = """\
 20180101 23:01:05.001|1001|101|98|25|20|99.9|TSTAT
@@ -18,7 +20,7 @@ TEST_INPUT = """\
 20180101 23:04:06.017|1001|101|98|25|20|89.9|TSTAT
 20180101 23:04:11.531|1000|17|15|9|8|7.9|BATT
 20180101 23:05:05.021|1001|101|98|25|20|89.9|TSTAT
-20180101 23:05:07.421|1001|17|15|9|8|7.9|BATT""".split("\n")
+20180101 23:05:07.421|1001|17|15|9|8|7.9|BATT"""
 
 TIMESTAMPS = [
     dt.datetime(2018, 1, 1, 23, 1, 5, 1000, dt.UTC),
@@ -38,9 +40,32 @@ TIMESTAMPS = [
 ]
 
 
+@pytest.fixture
+def test_data() -> list[tuple[int, int, int, int, int, float, Component]]:
+    """Test fixture for sample input."""
+    return [
+        (1001, 101, 98, 25, 20, 99.9, Component("TSTAT")),
+        (1000, 17, 15, 9, 8, 7.8, Component("BATT")),
+    ]
+
+
+@pytest.fixture
+def records(test_data: list[tuple[int, int, int, int, int, float, Component]]) -> list[Record]:
+    """Test fixture for parsed sample input."""
+    return [Record(ts, *x) for ts, x in zip(TIMESTAMPS, test_data)]
+
+
 @pytest.mark.parametrize(
-    "inp,expected", list((s.split("|", 1)[0], ans) for s, ans in zip(TEST_INPUT, TIMESTAMPS))
+    "inp,expected", list((s.split("|", 1)[0], ans) for s, ans in zip(TEST_INPUT.split("\n"), TIMESTAMPS))
 )
 def test_parse_timestamp(inp: str, expected: dt.datetime) -> None:
     """Test parsing the timestamps."""
     assert __main__.parse_timestamp(inp) == expected
+
+
+def test_read_records(tmp_path: Path, records: list[Record]) -> None:
+    """Test parsing the records."""
+    f = tmp_path / "test_data.txt"
+    f.write_text("\n".join(TEST_INPUT.split("\n")[:2]))
+
+    assert list(__main__.read_records(f)) == records

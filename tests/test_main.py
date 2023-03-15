17eea700329380cc7c09479bfed553c504daa063
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-03-14 22:22:44 -0400

    Add tests for filters and checks

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 
 from mission_control import __main__
-from mission_control.process_data import Component, Record
+from mission_control.process_data import Alert, Component, Record
 
 TEST_INPUT = """\
 20180101 23:01:05.001|1001|101|98|25|20|99.9|TSTAT
@@ -46,6 +46,18 @@ def test_data() -> list[tuple[int, int, int, int, int, float, Component]]:
     return [
         (1001, 101, 98, 25, 20, 99.9, Component("TSTAT")),
         (1000, 17, 15, 9, 8, 7.8, Component("BATT")),
+        (1001, 101, 98, 25, 20, 99.8, Component("TSTAT")),
+        (1000, 101, 98, 25, 20, 102.9, Component("TSTAT")),
+        (1000, 101, 98, 25, 20, 87.9, Component("TSTAT")),
+        (1001, 101, 98, 25, 20, 89.3, Component("TSTAT")),
+        (1001, 101, 98, 25, 20, 89.4, Component("TSTAT")),
+        (1000, 17, 15, 9, 8, 7.7, Component("BATT")),
+        (1000, 101, 98, 25, 20, 102.7, Component("TSTAT")),
+        (1000, 101, 98, 25, 20, 101.2, Component("TSTAT")),
+        (1001, 101, 98, 25, 20, 89.9, Component("TSTAT")),
+        (1000, 17, 15, 9, 8, 7.9, Component("BATT")),
+        (1001, 101, 98, 25, 20, 89.9, Component("TSTAT")),
+        (1001, 17, 15, 9, 8, 7.9, Component("BATT")),
     ]
 
 
@@ -66,6 +78,31 @@ def test_parse_timestamp(inp: str, expected: dt.datetime) -> None:
 def test_read_records(tmp_path: Path, records: list[Record]) -> None:
     """Test parsing the records."""
     f = tmp_path / "test_data.txt"
-    f.write_text("\n".join(TEST_INPUT.split("\n")[:2]))
+    f.write_text(TEST_INPUT)
 
     assert list(__main__.read_records(f)) == records
+
+
+def test_age(records: list[Record]) -> None:
+    """Test age filter."""
+    records = (
+        [Record(dt.datetime(2018, 1, 1, 22, tzinfo=dt.UTC), 1000, 20, 20, 20, 20, 20, Component("BATT"))] + records
+    )
+    # all entries are within 5 minutes of each other, except the one we just added
+    assert list(__main__.age(records)) == [False] + [True] * (len(records) - 1)
+
+
+def test_high_temp(records: list[Record]) -> None:
+    """Test high temp alert."""
+    assert (
+        __main__.high_temp([x for x in records if x.satellite_id == 1000])
+        == Alert(satelliteId=1000, severity="RED HIGH", component=Component("TSTAT"), timestamp=TIMESTAMPS[3])
+    )
+
+
+def test_low_voltage(records: list[Record]) -> None:
+    """Test low voltage alert."""
+    assert (
+        __main__.low_voltage([x for x in records if x.satellite_id == 1000])
+        == Alert(satelliteId=1000, severity="RED LOW", component=Component("BATT"), timestamp=TIMESTAMPS[1])
+    )
